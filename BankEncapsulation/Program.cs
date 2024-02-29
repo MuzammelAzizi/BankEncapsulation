ed4cfd344cb198dcede6bd8ccae6789a1e151c5c
Muzammel Osmani Azizi <ms7.swaggy@gmail.com>
2024-02-29 14:44:59 -0800

    status

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var userIsAccount = new BankAccount();
+
+            Console.WriteLine("how much $ you want to deposit?");
+
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            userIsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Your current balance is {userIsAccount.GetBalance()}");
         }
     }
 }
